fd6bcdb7b79bb8b46cf1c76f0f34046169bfba25
m.roppy <roppy@hotmail.co.jp>
2015-10-19 19:45:44 +0900

    Nelson式プロジェクトをインポート

---
+++ Assets/SCRIPT/CameraScript.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour {
+
+	public Material chosenObject;
+	private Material prevMaterial;
+	private GameObject prevGobj;
+
+	private float timerTemp;
+	public Text countDown;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = GetComponent<Camera> ().ViewportPointToRay (new Vector3 (0.5f,0.5f,0.0f));
+		
+		if (Physics.Raycast (ray, out hit)) {
+			Transform objectHit = hit.transform;
+
+			if (hit.transform.gameObject.layer == ConstantScript.CAMERA_LAYER || 
+			    hit.transform.gameObject.layer == ConstantScript.GOAL_LAYER) {
+				Debug.Log ("CAMERA HIT");
+				if (prevGobj && prevMaterial) {
+					this.prevGobj.GetComponent<Renderer> ().material = this.prevMaterial;
+				}
+				this.prevMaterial = hit.transform.gameObject.GetComponent<Renderer> ().material;
+
+				hit.transform.gameObject.GetComponent<Renderer> ().material = chosenObject;
+				this.prevGobj = hit.transform.gameObject;
+
+
+
+				this.timerTemp+=Time.deltaTime;
+
+				if(this.timerTemp >= ConstantScript.LOOK_DELAY){
+					countDown.gameObject.SetActive(true);
+					countDown.text = ""+(this.timerTemp-ConstantScript.LOOK_DELAY);
+				}
+
+				if(this.timerTemp >= (ConstantScript.LOOK_LENGTH+ConstantScript.LOOK_DELAY)){
+					this.timerTemp = 0.0f;
+					countDown.gameObject.SetActive(false);
+					countDown.text = "0.0";
+					this.Jump(hit.transform.gameObject);
+				}
+			}
+
+			
+			//Debug.Log("test "+hit.transform.gameObject.layer);
+
+			// Do something with the object that was hit by the raycast.
+		} 
+		else {
+			if(this.prevGobj){
+				if(this.prevGobj.layer == ConstantScript.CAMERA_LAYER || 
+				   this.prevGobj.layer == ConstantScript.GOAL_LAYER){
+					if (prevGobj && prevMaterial) {
+						this.prevGobj.GetComponent<Renderer> ().material = this.prevMaterial;
+					}
+					this.prevGobj = null;
+					this.prevMaterial = null;
+					this.timerTemp = 0.0f;
+					countDown.gameObject.SetActive(false);
+					countDown.text = "0.0";
+				}
+			}
+
+		}
+
+	}
+
+	public void Jump(GameObject target){
+		if (target) {
+			if(target.layer == ConstantScript.CAMERA_LAYER){
+				Transform temp = target.transform.GetChild(0);
+				this.transform.position = target.transform.position;
+				this.transform.rotation = temp.rotation;
+			}
+			else if(target.layer == ConstantScript.GOAL_LAYER){
+				GameSceneHandler.gameFlag = GameSceneHandler.GAME_STATUS.GAME_OVER;
+			}
+
+		}
+	}
+
+	public void MoveUp(float speed, float deltaTime){
+		this.transform.Rotate(Vector3.left * speed * deltaTime);
+	}
+
+	public void MoveDown(float speed, float deltaTime){
+		this.transform.Rotate(Vector3.right * speed * deltaTime);
+	}
+
+	public void MoveLeft(float speed, float deltaTime){
+		this.transform.Rotate(Vector3.down * speed * deltaTime);
+	}
+
+	public void MoveRight(float speed, float deltaTime){
+		this.transform.Rotate(Vector3.up * speed * deltaTime);
+	}
+}
---
+++ Assets/SCRIPT/ConstantScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public static class ConstantScript
+{
+	////////カメラの設定////////
+	//カメラのスピード
+	public const float CAMERA_SPEED = 80.0f;
+
+	public const int CAMERA_LAYER = 8;
+	public const int GOAL_LAYER = 9;
+
+	public const float LOOK_LENGTH = 4.0f;
+	public const float LOOK_DELAY = 1.0f;
+
+	public const string SCENE_INGAME = "Main";
+}
